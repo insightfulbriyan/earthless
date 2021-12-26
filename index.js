0c9366745e498ccf46daacafbe04f92f99f276d9
Jaka <vesoljec.jk@gmail.com>
2021-12-26 23:52:25 +0100

    what about now

--- index.js
+++ index.js
@@ -331,10 +331,10 @@ client.on("interactionCreate", async interaction => {
 
     //MUTE 
     else if (interaction.commandName == "mute" && interaction.memberPermissions.has("KICK_MEMBERS")) {
-        var member = interaction.guild.members.fetch(interaction.options.getUser("user").id).roles
-        member.add(CONFIG.MUTE_ID)
-        setTimeout(() => { member.remove(CONFIG.MUTE_ID) }, CONFIG.MUTE_TIME)
-        interaction.reply("**" + interaction.options.getUser("user").displayName.toString() + "** has been muted for 5 minutes.")
+        var member = interaction.guild.members.fetch(interaction.options.getUser("user").id)
+        member.roles.add(CONFIG.MUTE_ID)
+        setTimeout(() => { member.roles.remove(CONFIG.MUTE_ID) }, CONFIG.MUTE_TIME)
+        interaction.reply("**" + member.displayName.toString() + "** has been muted for 5 minutes.")
     }
 
 
